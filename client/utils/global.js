4713613197ee50144a954ec1d12e271887c6c6e1
Eyas <liuyuesongde@163.com>
2016-08-11 00:03:37 +0800

    fetch function

--- client/utils/global.js
+++ client/utils/global.js
@@ -11,13 +11,23 @@ import Router from 'react-router/lib/Router';
 import Route from 'react-router/lib/Route';
 import IndexRedirect from 'react-router/lib/IndexRedirect';
 
-(function(objs){
+import fetch from './fetch';
+
+/**
+ * 设置全局变量
+ * @param  {object} objs，foucs 需要设置为全局变量的对象集合，objs安全设置，foucs强制设置
+ * @return {[type]}            [description]
+ */
+(function(objs, foucs){
   for(let key in objs){
     if(!window.hasOwnProperty(key)){
       window[key] = objs[key];
     }else{
       console.error("global variable ["+key+"] is already exits!")
     }
+  }
+  for(let key in foucs){
+    window[key] = foucs[key]
   }
 })({
   React, 
@@ -33,4 +43,7 @@ import IndexRedirect from 'react-router/lib/IndexRedirect';
   Router,
   Route,
   IndexRedirect
+}, {
+  fetch,
+  _
 })
