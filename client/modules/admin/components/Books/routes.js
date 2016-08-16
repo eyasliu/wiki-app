d223bd335f4b3a7ec48c7c2f76ae564ac1dd2b64
Eyas <liuyuesongde@163.com>
2016-08-17 00:02:57 +0800

    Signed-off-by: Eyas <liuyuesongde@163.com>

--- client/modules/admin/components/Books/routes.js
+++ client/modules/admin/components/Books/routes.js
@@ -6,6 +6,7 @@ import Tags from '../Tags'
 export default (
 <Route path="books" component={Books}>
   <Route path="list" component={List}></Route>
+  <Route path="edit/:id" component={Edit}></Route>
   <Route path="add" component={Edit}></Route>
   <Route path="tags" component={Tags}></Route>
   <IndexRoute component={List} />
